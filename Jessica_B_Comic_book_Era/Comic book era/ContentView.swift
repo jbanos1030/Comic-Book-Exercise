8976bfa46d292340ee704892722971678c89a9e1
jbanos1030 <67976657+jbanos1030@users.noreply.github.com>
2020-07-30 14:36:49 -0500

    Add files via upload

---
+++ Jessica_B_Comic_book_Era/Comic book era/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Comic book era
+//
+//  Created by Jessica Banos on 7/29/20.
+//  Copyright © 2020 Jessica Banos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jessica_B_Comic_book_Era/Comic book eraTests/Comic_book_eraTests.swift
@@ -0,0 +1,34 @@
+//
+//  Comic_book_eraTests.swift
+//  Comic book eraTests
+//
+//  Created by Jessica Banos on 7/29/20.
+//  Copyright © 2020 Jessica Banos. All rights reserved.
+//
+
+import XCTest
+@testable import Comic_book_era
+
+class Comic_book_eraTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
